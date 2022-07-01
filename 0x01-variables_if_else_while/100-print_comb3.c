25df6dfd69879b1df02813075bde2677b6d2795d
SkilfulMan <princeay1515@gmail.com>
2022-07-01 13:06:40 -0700

    Write a program that prints all possible different combinations of two digits.
    Numbers must be separated by ,, followed by a space
    The two digits must be different
    01 and 10 are considered the same combination of the two digits 0 and 1
    Print only the smallest combination of two digits
    Numbers should be printed in ascending order, with two digits
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    You can only use putchar five times maximum in your code
    You are not allowed to use any variable of type char
    All your code should be in the main function

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- *  * main - program that prints all possible different combinations of two digits
- *   * Numbers must be separated by ,followed by a space
- *    * The two digits must be different
- *     * 01 and 10 are considered the same combination of the two digits 0 and 1
- *      * Return: 0
- *       */
+ * main - program that prints all possible different combinations of two digits
+ * Numbers must be separated by ,followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return: 0
+ */
 
 int main(void)
 {
 	int n1 = 48;
 	int n2;
 	int com = 44;
+
 	while (n1 <= 57)
 	{
 		n2 = n1 + 1;
