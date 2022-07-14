177f00b97cc65710a106be2147208e94fd7ec88c
SkilfulMan <princeay1515@gmail.com>
2022-07-14 16:11:55 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.
    You are not allowed to use the variable a in your new line of code
    You are not allowed to modify the variable p
    You can only write one statement
    You are not allowed to use ,
    You are not allowed to code anything else than the line of expected line of code at the expected line
    Your code should be written at line 19, before the ;
    Do not remove anything from the initial code (not even the comments)
    and don’t change anything but the line of code you are adding (don’t change the spaces to tabs!)
    You are allowed to use the standard library

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-
+/**
+ * main - Add one line to this code, so that the program
+ * prints a[2] = 98, followed by a new line.
+ * Return: Always
+ */
 int main(void)
 {
 	int n;
@@ -9,19 +12,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-<<<<<<< HEAD
+	 /*
+	  *  * write your line of code here...
+	  *   * Remember:
+	  *    * - you are not allowed to use a
+	  *     * - you are not allowed to modify p
+	  *      * - only one statement
+	  *       * - you are not allowed to code anything else than this line of code
+	  */
 	*(p + 5) = 98;
-=======
-	*(p +5) = 98;
->>>>>>> 279d7b82084df413f176c90be7f9716fa2f396c8
+
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
