cdcbec4e376e2cf0b33ea3e6bc2df050e553fb47
SkilfulMan <princeay1515@gmail.com>
2022-07-14 16:02:10 -0700

    Write a function that adds two numbers.
    Prototype: char *infinite_add(char *n1, char *n2, char *r, int size_r);
    Where n1 and n2 are the two numbers
    r is the buffer that the function will use to store the result
    size_r is the buffer size
    The function returns a pointer to the result
    You can assume that you will always get positive numbers, or 0
    You can assume that there will be only digits in the strings n1 and n2
    n1 and n2 will never be empty
    If the result can not be stored in r the function must return 0

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,10 +22,10 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
-/*
+/**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
- * : text representation of 2nd number to add
+ * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
