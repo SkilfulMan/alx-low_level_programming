6095e90dcf5f8b7c4c8ba92973abbf43ab252d6d
SkilfulMan <princeay1515@gmail.com>
2022-07-20 09:55:45 -0700

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.
    Prototype: int is_prime_number(int n);

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,11 +11,11 @@
 int prime_check(int f, int p)
 {
 		if (p < 2 || p % f == 0)
-					return (0);
-			else if (f > p / 2)
-						return (1);
-				else
-							return (prime_check(f + 1, p));
+			return (0);
+		else if (f > p / 2)
+			return (1);
+		else
+			return (prime_check(f + 1, p));
 }
 
 /**
@@ -27,7 +27,7 @@ int prime_check(int f, int p)
 
 int is_prime_number(int n)
 {
-		if (n == 2)
-					return (1);
-			return (prime_check(2, n));
+	if (n == 2)
+		return (1);
+	return (prime_check(2, n));
 }
